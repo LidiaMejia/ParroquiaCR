adea6ccda56495aee8c6a9bba359a40ae4fc4bc5
SDM <stephan.1998@outlook.com>
2020-04-08 23:24:22 -0600

    Carretillas

--- docs/scripts/04_create_productos.sql
+++ docs/scripts/04_create_productos.sql
@@ -12,4 +12,32 @@ CREATE TABLE `parroquia`.`productos` (
   PRIMARY KEY (`codprd`),
   UNIQUE INDEX `skuprd_UNIQUE`(`skuprd` ASC)); 
 
+CREATE TABLE `dynapolls`.`carretilla` (
+  `usercod` BIGINT(10) NOT NULL,
+  `codprd` BIGINT(18) NOT NULL,
+  `crrctd` INT(5) NOT NULL,
+  `crrprc` DECIMAL(12,2) NOT NULL,
+  `crrfching` DATETIME NOT NULL,
+  PRIMARY KEY (`usercod`, `codprd`),
+  INDEX `codprd_idx` (`codprd` ASC),
+  CONSTRAINT `carretilla_user_key`
+    FOREIGN KEY (`usercod`)
+    REFERENCES `dynapolls`.`usuario` (`usercod`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `carretilla_prd_key`
+    FOREIGN KEY (`codprd`)
+    REFERENCES `dynapolls`.`productos` (`codprd`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
 
+CREATE TABLE `carretillaanon` (
+  `anoncod` varchar(128) NOT NULL,
+  `codprd` bigint(18) NOT NULL,
+  `crrctd` int(5) NOT NULL,
+  `crrprc` decimal(12,2) NOT NULL,
+  `crrfching` datetime NOT NULL,
+  PRIMARY KEY (`anoncod`,`codprd`),
+  KEY `codprd_idx` (`codprd`),
+  CONSTRAINT `carretillaanon_prd_key` FOREIGN KEY (`codprd`) REFERENCES `productos` (`codprd`) ON DELETE NO ACTION ON UPDATE NO ACTION
+);
