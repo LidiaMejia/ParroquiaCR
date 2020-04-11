1ef350aa3fce2fd69e64b6195c507d9fbe1ffbfa
Diana Palma <delizabethpalma@hotmail.com>
2020-04-11 15:46:57 -0600

    Cambios de producto.model

--- models/productos.model.php
+++ models/productos.model.php
@@ -23,7 +23,7 @@ function getUnAuthTimeDelta()
 
 function getOneProductoCatalogo($codprd)
 {
-    $sqlSelect = "SELECT codprd, dscprd, stkprd, skuprd, urlthbprd, prcprd
+    $sqlSelect = "SELECT codprd, dscprd, skuprd, urlthbprd, prcprd
         from productos where  codprd=%d;";
     $tmpProducto =  obtenerRegistros(sprintf($sqlSelect, $codprd));
     $assocProducto = array();
@@ -49,13 +49,7 @@ function getOneProductoCatalogo($codprd)
             $codprd
         )
     );
-    foreach ($arrReserved as $reserved) {
-        if (isset($assocProducto[$reserved["codprd"]])) {
-            $assocProducto[$reserved["codprd"]]["stkprd"]
-                = $assocProducto[$reserved["codprd"]]["stkprd"]
-                  - $reserved["reserved"];
-        }
-    }
+   
   
     $timeDelta = getUnAuthTimeDelta();
     $sqlSelectReserved = "select codprd, sum(crrctd) as reserved
@@ -70,13 +64,8 @@ function getOneProductoCatalogo($codprd)
             $codprd
         )
     );
-    foreach ($arrReserved as $reserved) {
-        if (isset($assocProducto[$reserved["codprd"]])) {
-            $assocProducto[$reserved["codprd"]]["stkprd"]
-                = $assocProducto[$reserved["codprd"]]["stkprd"]
-                  - $reserved["reserved"];
-        }
-    }
+  
+
     if (count($assocProducto)) {
         return $assocProducto[$codprd];
     } else {
@@ -90,8 +79,8 @@ function addToAutCart($codprod, $usuario, $cantidad, $precio)
 {
     $productoCart = getOneProductoCatalogo($codprod);
     error_log(json_encode($productoCart));
+    
     if (count($productoCart)) {
-        if ($productoCart["stkprd"] >= $cantidad) {
             $sqlins = "INSERT INTO `carretilla`
             (`usercod`, `codprd`, `crrctd`, `crrprc`, `crrfching`)
             VALUES (%d, %d, %d, %f, now())
@@ -107,7 +96,6 @@ function addToAutCart($codprod, $usuario, $cantidad, $precio)
                     $precio
                 )
             );
-        }
     }
     return 0;
 }
@@ -330,6 +318,191 @@ function passAnonCartToAutCart($uniqueUser, $user)
     );
     terminarTransaccion();
     return getCartProducts($user);
+}
+
+function todosLosProductos()
+{
+    $sqlSelect = "SELECT * FROM productos;";
+
+    return obtenerRegistros($sqlSelect);
+}
+
+function productoCatalogo()
+{
+    $sqlSelect = "SELECT codprd, dscprd, skuprd, urlthbprd, prcprd
+        from productos where estprd in('ACT','DSC');";
+    $tmpProducto =  obtenerRegistros($sqlSelect);
+    $assocProducto = array();
+    foreach ($tmpProducto as $producto) {
+        //Imagen predeterminada si no hay imagen
+        $assocProducto[$producto["codprd"]] = $producto;
+        if (preg_match('/^\s*$/', $producto["urlthbprd"])) {
+            $assocProducto[$producto["codprd"]]["urlthbprd"]
+                = "public/imgs/noprodthb.png"; //Insertar la direccion de la imagen------------
+        }
+    }
+    
+    $timeDelta =  getAuthTimeDelta();
+    $sqlSelectReserved = "select codprd, sum(crrctd) as reserved
+    from carretilla where TIME_TO_SEC(TIMEDIFF(now(), crrfching)) <= %d
+    group by codprd;
+    ";
+    $arrReserved = obtenerRegistros(
+        sprintf(
+            $sqlSelectReserved,
+            $timeDelta
+        )
+    );
+    
+    
+    $timeDelta = getUnAuthTimeDelta(); 
+    $sqlSelectReserved = "select codprd, sum(crrctd) as reserved
+    from carretillaanon where TIME_TO_SEC(TIMEDIFF(now(), crrfching)) <= %d
+    group by codprd;
+    ";
+    $arrReserved = obtenerRegistros(
+        sprintf(
+            $sqlSelectReserved,
+            $timeDelta
+        )
+    );
+  
+    return $assocProducto;
+}
+
+function insertProducto($dscprd, $sdscprd, $ldscprd, $skuprd, $catprd, $prcprd,
+    $urlprd, $urlthbprd, $estprd) 
+{
+    $sqlInsert = "INSERT INTO productos (dscprd, sdscprd, ldscprd, skuprd,
+        catprd, prcprd, urlprd, urlthbprd, estprd)
+        VALUES ('%s', '%s', '%s', '%s', '%s', %lf, '%s', '%s', '%s');";
+
+    $isOk = ejecutarNonQuery(
+        sprintf(
+            $sqlInsert,
+            $dscprd,
+            $sdscprd,
+            $ldscprd,
+            $skuprd,
+            $catprd,
+            $prcprd,
+            '',
+            '',
+            $estprd
+        )
+    );
+    return getLastInserId();
+}
+
+function updateProducto($dscprd, $sdscprd, $ldscprd, $skuprd, $catprd, $prcprd,
+    $urlprd, $urlthbprd, $estprd, $codprd) 
+{
+    $sqlUpdate = "UPDATE productos SET dscprd = '%s', sdscprd = '%s',
+        ldscprd = '%s', skuprd = '%s', catprd = '%s', prcprd = %lf, 
+        estprd = '%s' WHERE codprd = %d;";
+
+    return ejecutarNonQuery(
+        sprintf(
+            $sqlUpdate,
+            $dscprd,
+            $sdscprd,
+            $ldscprd,
+            $skuprd,
+            $catprd,
+            $prcprd,
+            $estprd,
+            $codprd
+        )
+    );
+}
+
+function deleteProducto($codprd)
+{
+    $sqlDelete = "DELETE FROM productos WHERE codprd = %d;";
+
+    return ejecutarNonQuery(
+        sprintf($sqlDelete, $codprd)
+    );
+}
+
+//COLOCAR IMAGEN DEL PRODUCTO
+function setImageProducto($url, $codprd, $type="PRT")
+{
+    $sqlUpdatePRT = "UPDATE productos SET urlprd = '%s' WHERE codprd = %d;";
+    $sqlUpdateTHB = "UPDATE productos SET urlthbprd = '%s' WHERE codprd = %d;";
+    $sqlUpdate = ($type === "PRT") ? $sqlUpdatePRT : $sqlUpdateTHB;
+    return ejecutarNonQuery(
+        sprintf(
+            $sqlUpdate,
+            $url,
+            $codprd
+        )
+    );
+}
+
+function getDetailCartAut($usuario)
+{
+    $sqlstr = "
+      select a.codprd, b.skuprd, b.dscprd, a.crrctd, a.crrprc
+      from `carretilla` a inner join `productos` b on a.codprd = b.codprd
+      where a.usercod = %d and TIME_TO_SEC(TIMEDIFF(now(), a.crrfching)) <= %d;
+    ";
+    $arrProductos = obtenerRegistros(
+        sprintf(
+            $sqlstr,
+            $usuario,
+            getAuthTimeDelta()
+        )
+    );
+    $arrProductosFinal = array();
+    $arrProductosFinal["products"] = array();
+    $arrProductosFinal["totctd"] = 0;
+    $arrProductosFinal["total"] = 0;
+    $counter = 1;
+    foreach ($arrProductos as $producto) {
+        $producto["line"] = $counter;
+        $producto["total"]
+            = number_format(
+                $producto["crrctd"] * $producto["crrprc"],
+                2
+            );
+        $arrProductosFinal["totctd"] += $producto["crrctd"];
+        $arrProductosFinal["total"] += ($producto["crrctd"] * $producto["crrprc"]);
+        $arrProductosFinal["products"][] = $producto;
+        $counter ++;
+    }
+    $arrProductosFinal["total"] = number_format($arrProductosFinal["total"], 2);
+    return $arrProductosFinal;
+}
+
+function cleanTimeOutCart()
+{
+    $contador = 0;
+    iniciarTransaccion();
+
+    //CARRETILLA ANONIMA
+    $sqlDel = "DELETE from carretillaanon
+      where TIME_TO_SEC(TIMEDIFF(now(), crrfching)) > %d";
+
+    $contador += ejecutarNonQuery(
+        sprintf(
+            $sqlDel,
+            getUnAuthTimeDelta()
+        )
+    );
+
+    // CARRETILLA AUTENTICADA
+    $sqlDel = "DELETE from carretilla
+      where TIME_TO_SEC(TIMEDIFF(now(), crrfching)) > %d";
+
+    $contador += ejecutarNonQuery(
+        sprintf(
+            $sqlDel,
+            getAuthTimeDelta()
+        )
+    );
+    terminarTransaccion();
+    return $contador;
 }
 
 
