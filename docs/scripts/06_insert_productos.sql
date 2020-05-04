81ddb94195e9d47fda66684524b35d65e71b271a
Lidia Mejia <lidiamejia29@yahoo.com>
2020-05-03 22:18:44 -0600

    Script Insert Productos

---
+++ docs/scripts/06_insert_productos.sql
@@ -0,0 +1,107 @@
+INSERT INTO `productos`
+(`codprd`, `dscprd`, `sdscprd`, `ldscprd`, `skuprd`, `catprd`, `prcprd`, `urlprd`, `urlthbprd`, `estprd`)
+VALUES
+(3, 'San Antonio de Padua', 'Insignia de San Antonio de Padua', 'Insignia dorada de San Antonio de Padua con una flor de lirio', 'INSIG001', 
+'LBA', 150.00, 'public/produc/3_InsgPadua.png', 'public/produc/3_InsgPadua_15x15.png', 'ACT');
+
+INSERT INTO `productos`
+(`codprd`, `dscprd`, `sdscprd`, `ldscprd`, `skuprd`, `catprd`, `prcprd`, `urlprd`, `urlthbprd`, `estprd`)
+VALUES
+(4, 'San Francisco de Asis', 'Insignia de San Francisco de Asis', 'Insignia color cobre de San Francisco de Asis con una cruz Tau', 'INSIG002', 
+'LBA', 130.00, 'public/produc/4_InsgFrancisco.png', 'public/produc/4_InsgFrancisco_15x15.png', 'ACT');
+
+INSERT INTO `productos`
+(`codprd`, `dscprd`, `sdscprd`, `ldscprd`, `skuprd`, `catprd`, `prcprd`, `urlprd`, `urlthbprd`, `estprd`)
+VALUES
+(5, 'María Reina de la Paz', 'Insignia de María Reina de la Paz', 'Insignia azul celeste de la Virgen María Reina de la Paz', 'INSIG003', 
+'LBA', 100.00, 'public/produc/5_InsgReinaPaz.png', 'public/produc/5_InsgReinaPaz_15x15.png', 'ACT');
+
+INSERT INTO `productos`
+(`codprd`, `dscprd`, `sdscprd`, `ldscprd`, `skuprd`, `catprd`, `prcprd`, `urlprd`, `urlthbprd`, `estprd`)
+VALUES
+(6, 'San Ignacio de Loyola', 'Insignia de San Ignacio de Loyola', 'Insignia plateada de San Ignacio de Loyola', 'INSIG004', 
+'COM', 160.00, 'public/produc/6_InsgLoyola.png', 'public/produc/6_InsgLoyola_15x15.png', 'ACT');
+
+INSERT INTO `productos`
+(`codprd`, `dscprd`, `sdscprd`, `ldscprd`, `skuprd`, `catprd`, `prcprd`, `urlprd`, `urlthbprd`, `estprd`)
+VALUES
+(7, 'San Judas Tadeo', 'Insignia de San Judas Tadeo', 'Insignia redonda y verde de San Judas Tadeo', 'INSIG005', 
+'COM', 170.00, 'public/produc/7_InsgTadeo.png', 'public/produc/7_InsgTadeo_15x15.png', 'ACT');
+
+INSERT INTO `productos`
+(`codprd`, `dscprd`, `sdscprd`, `ldscprd`, `skuprd`, `catprd`, `prcprd`, `urlprd`, `urlthbprd`, `estprd`)
+VALUES
+(8, 'Nuestra Señora de Lourdes', 'Insignia de Nuestra Señora de Lourdes', 'Insignia azul celeste de Nuestra Señora de Lourdes', 'INSIG006', 
+'COM', 150.00, 'public/produc/8_InsgLourdes.png', 'public/produc/8_InsgLourdes_15x15.png', 'ACT');
+
+INSERT INTO `productos`
+(`codprd`, `dscprd`, `sdscprd`, `ldscprd`, `skuprd`, `catprd`, `prcprd`, `urlprd`, `urlthbprd`, `estprd`)
+VALUES
+(9, 'San Pío de Pietrelcina', 'Insignia de San Pío de Pietrelcina', 'Insignia color cobre de San Pío de Pietrelcina', 'INSIG007', 
+'CLN', 180.00, 'public/produc/9_InsgPio.png', 'public/produc/9_InsgPio_15x15.png', 'ACT');
+
+INSERT INTO `productos`
+(`codprd`, `dscprd`, `sdscprd`, `ldscprd`, `skuprd`, `catprd`, `prcprd`, `urlprd`, `urlthbprd`, `estprd`)
+VALUES
+(10, 'Santo Papa Pablo VI', 'Insignia del Santo Papa Pablo VI', 'Insignia dorada del Santo Papa Pablo VI', 'INSIG008', 
+'CLN', 120.00, 'public/produc/10_InsgPabloVI.png', 'public/produc/10_InsgPabloVI_15x15.png', 'ACT');
+
+INSERT INTO `productos`
+(`codprd`, `dscprd`, `sdscprd`, `ldscprd`, `skuprd`, `catprd`, `prcprd`, `urlprd`, `urlthbprd`, `estprd`)
+VALUES
+(11, 'Virgen de Guadalupe', 'Insignia de la Virgen de Guadalupe', 'Insignia verde de la Virgen de Guadalupe', 'INSIG009', 
+'CLN', 300.00, 'public/produc/11_InsgGuadalupe.png', 'public/produc/11_InsgGuadalupe_15x15.png', 'ACT');
+
+INSERT INTO `productos`
+(`codprd`, `dscprd`, `sdscprd`, `ldscprd`, `skuprd`, `catprd`, `prcprd`, `urlprd`, `urlthbprd`, `estprd`)
+VALUES
+(12, 'San Martín de Porres', 'Insignia de San Martín de Porres', 'Insignia azul oscuro de San Martín de Porres', 'INSIG010', 
+'OBS', 125.00, 'public/produc/12_InsgPorres.png', 'public/produc/12_InsgPorres_15x15.png', 'ACT');
+
+INSERT INTO `productos`
+(`codprd`, `dscprd`, `sdscprd`, `ldscprd`, `skuprd`, `catprd`, `prcprd`, `urlprd`, `urlthbprd`, `estprd`)
+VALUES
+(13, 'Santa Teresa de Calcuta', 'Insignia de Santa Teresa de Calcuta', 'Insignia redonda azul oscuro de Santa Teresa de Calcuta con la Cruz de la Unidad', 'INSIG011', 
+'OBS', 220.00, 'public/produc/13_InsgCalcuta.png', 'public/produc/13_InsgCalcuta_15x15.png', 'ACT');
+
+INSERT INTO `productos`
+(`codprd`, `dscprd`, `sdscprd`, `ldscprd`, `skuprd`, `catprd`, `prcprd`, `urlprd`, `urlthbprd`, `estprd`)
+VALUES
+(14, 'Virgen del Carmen', 'Insignia de la Virgen del Carmen', 'Insignia café de la Virgen del Carmen', 'INSIG012', 
+'OBS', 130.00, 'public/produc/14_InsgCarmen.png', 'public/produc/14_InsgCarmen_15x15.png', 'ACT');
+
+INSERT INTO `productos`
+(`codprd`, `dscprd`, `sdscprd`, `ldscprd`, `skuprd`, `catprd`, `prcprd`, `urlprd`, `urlthbprd`, `estprd`)
+VALUES
+(15, 'Santo Papa Juan Pablo II', 'Insignia del Santo Papa Juan Pablo II', 'Insignia roja del Santo Papa Juan Pablo II', 'INSIG013', 
+'REM', 150.00, 'public/produc/15_InsgJPII.png', 'public/produc/15_InsgJPII_15x15.png', 'ACT');
+
+INSERT INTO `productos`
+(`codprd`, `dscprd`, `sdscprd`, `ldscprd`, `skuprd`, `catprd`, `prcprd`, `urlprd`, `urlthbprd`, `estprd`)
+VALUES
+(16, 'Santa Teresita del Niño Jesús', 'Insignia de Santa Teresita del Niño Jesús', 'Insignia rosa de Santa Teresita del Niño Jesús', 'INSIG014', 
+'REM', 115.00, 'public/produc/16_InsgTeresita.png', 'public/produc/16_InsgTeresita_15x15.png', 'ACT');
+
+INSERT INTO `productos`
+(`codprd`, `dscprd`, `sdscprd`, `ldscprd`, `skuprd`, `catprd`, `prcprd`, `urlprd`, `urlthbprd`, `estprd`)
+VALUES
+(17, 'Virgen de Suyapa', 'Insignia de la Virgen de Suyapa', 'Insignia de la Virgen de Suyapa con los colores de la bandera de Honduras', 'INSIG015', 
+'REM', 190.00, 'public/produc/17_InsgSuyapa.png', 'public/produc/17_InsgSuyapa_15x15.png', 'ACT');
+
+INSERT INTO `productos`
+(`codprd`, `dscprd`, `sdscprd`, `ldscprd`, `skuprd`, `catprd`, `prcprd`, `urlprd`, `urlthbprd`, `estprd`)
+VALUES
+(18, 'San Juan Bosco', 'Insignia de San Juan Bosco', 'Insignia plateada de San Juan Bosco', 'INSIG016', 
+'ESC', 140.00, 'public/produc/18_InsgBosco.png', 'public/produc/18_InsgBosco_15x15.png', 'ACT');
+
+INSERT INTO `productos`
+(`codprd`, `dscprd`, `sdscprd`, `ldscprd`, `skuprd`, `catprd`, `prcprd`, `urlprd`, `urlthbprd`, `estprd`)
+VALUES
+(19, 'San Óscar Romero', 'Insignia de San Óscar Romero de El Salvador', 'Insignia púrpura de San Óscar Romero de El Salvador', 'INSIG017', 
+'ESC', 190.00, 'public/produc/19_InsgRomero.png', 'public/produc/19_InsgRomero_15x15.png', 'ACT');
+
+INSERT INTO `productos`
+(`codprd`, `dscprd`, `sdscprd`, `ldscprd`, `skuprd`, `catprd`, `prcprd`, `urlprd`, `urlthbprd`, `estprd`)
+VALUES
+(20, 'Virgen de Fátima', 'Insignia de la Virgen de Fátima', 'Insignia dorada de la Virgen de Fátima', 'INSIG018', 
+'ESC', 135.00, 'public/produc/20_InsgFatima.png', 'public/produc/20_InsgFatima_15x15.png', 'ACT');
